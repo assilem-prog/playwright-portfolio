a0325e6333de1e10d6fa47adbb47ce2b953c169a
assilem-prog <melissa.seneron.pro@gmail.com>
2026-09-22 19:53:13 +0200

    Add cart product removal action

--- pages/cart.page.ts
+++ pages/cart.page.ts
@@ -7,15 +7,20 @@ export class CartPage {
   // Titre "Your Cart" affiché en haut du panier.
   readonly title: Locator;
 
-  // Noms des produits
+  // Ensemble des produits présents dans le panier.
+  readonly productItems: Locator;
+
+  // Noms des produits.
   readonly inventoryItemNames: Locator;
 
-  // Prix des produits
+  // Prix des produits.
   readonly inventoryItemPrices: Locator;
 
-  // Quantité des produits
+  // Quantité des produits.
   readonly itemQuantity: Locator;
 
+  // Pastille qui affiche le nombre d'articles dans le panier.
+  readonly shoppingCartBadge: Locator;
 
   // Le constructeur reçoit la page navigateur fournie par Playwright.
   constructor(private page: Page) {
@@ -23,21 +28,25 @@ export class CartPage {
     // Titre principal de la page Panier.
     this.title = page.locator('[data-test="title"]');
 
+    // Chaque élément correspondant à un produit présent dans le panier.
+    this.productItems = page.locator('[data-test="inventory-item"]');
 
-
-    // Noms des produits
+    // Noms des produits.
     this.inventoryItemNames = page.locator('[data-test="inventory-item-name"]');
 
-    // Prix des produits
+    // Prix des produits.
     this.inventoryItemPrices = page.locator('[data-test="inventory-item-price"]');
 
-    // Quantité des produits
+    // Quantité des produits.
     this.itemQuantity = page.locator('[data-test="item-quantity"]');
 
-
+    // Pastille qui affiche le nombre d'articles dans le panier.
+    this.shoppingCartBadge = page.locator('[data-test="shopping-cart-badge"]');
   }
 
-
-
-
+  // Méthode qui permet de retirer un produit du panier en prenant son nom en paramètre.
+  async removeFromCart(productToRemove: string): Promise<void> {
+    const product = this.productItems.filter({ hasText: productToRemove });
+    await product.getByRole('button', { name: 'Remove' }).click();
+  }
 }
